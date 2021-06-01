d5edab687e8d03f5ebb6466d8048bc92fbc93c8a
Quanquan <qliu2011@gmail.com>
2021-06-01 18:01:42 -0400

    Revert "all code"
    This reverts commit f4a1d97b2cbe6362e2d9eca12e4e80940cf07760.

--- gbbs/gbbs.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "bridge.h"
-#include "graph.h"
-#include "interface.h"
-#include "macros.h"
-#include "parse_command_line.h"
-#include "vertex_subset.h"
-#include "benchmark.h"
