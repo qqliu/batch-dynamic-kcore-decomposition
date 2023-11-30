2a08cddecfbb6ba7397f10ad53ae2b6f15f96db9
Quanquan Liu <qliu2011@gmail.com>
2023-11-29 23:01:44 -0800

    setup bash script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+command -v make >/dev/null 2>&1 || { sudo apt install make }
+sudo apt-get update
+command -v g++ >/dev/null 2>&1 || { sudo apt install g++ }
+git submodule update –init –recursive
