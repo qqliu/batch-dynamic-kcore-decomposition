954e4b55d4ca99d44aa085ac8bfaf78ff32bb358
Quanquan Liu <qliu2011@gmail.com>
2023-11-30 11:37:00 +0000

    add experiment files

---
+++ gbbs/scripts/cplds_experiments/run_experiments.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Running experiments";
+echo "-------------------------------";
+
+echo "Latency experiments: average latency, 99% latency, 99.99% latency"
+echo "-------------------------------";
+python3 latency_test.py
+python3 latency_read.py
+echo "-------------------------------";
+echo "Update time experiments: average update time and max update time"
+echo "-------------------------------";
+
+python3 update_time_read.py
+echo "-------------------------------";
+echo "Approximation experiments: average approximation factor and max approximation factors"
+echo "-------------------------------";
+
+python3 approx_test.py
+python3 approx_read.py
+echo "-------------------------------";
+echo "Scalability of Read Throughputs"
+echo "-------------------------------";
+python3 read_test.py
+python3 read_read.py
+echo "-------------------------------";
+echo "Scalability of Write Throughputs"
+echo "-------------------------------";
+python3 write_test.py
+python3 write_read.py
+echo "-------------------------------";
