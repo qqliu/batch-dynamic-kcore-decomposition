03547d6d0b21bf9806c4fdf57e52412c964ef0bd
Quanquan Liu <qliu2011@gmail.com>
2023-11-30 07:10:32 +0000

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-command -v make >/dev/null 2>&1 || { sudo apt install make }
+command -v make >/dev/null 2>&1 || { sudo apt install make; }
 sudo apt-get update
-command -v g++ >/dev/null 2>&1 || { sudo apt install g++ }
-git submodule update –init –recursive
+command -v g++ >/dev/null 2>&1 || { sudo apt install g++; }
+
+git submodule update --init --recursive
