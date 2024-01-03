56fbd60bea382197c2b4d64300aa81f4ff2743c0
Quanquan Liu <qliu2011@gmail.com>
2024-01-02 22:34:23 -0500

    et tree

---
+++ gbbs/benchmarks/BatchDynamicConnectivity/EulerTourTree/ETTree.cc
@@ -0,0 +1,46 @@
+// Usage (dynamic):
+// numactl -i all ./LDS -rounds 3 -s -eps 0.4 -delta 3 -i <dynamic graph> -b 1000 <static graph>
+// flags:
+//   required:
+//     -s : indicates that the graph is symmetric
+//   optional:
+//     -m : indicate that the graph should be mmap'd
+//     -c : indicate that the graph is compressed
+//     -rounds : the number of times to run the algorithm
+//     -i : file path to the dynamic graph, with a single dynamic edge on each
+//          line, in the format "<+/-> u v", where + denotes insertion, -
+//          denotes deletion, and (u, v) are the vertex ids denoting the edge
+//          (e.g., "+ 0 1")
+//     -b : batch size (output includes time per batch, from start_size to
+//          end_size)
+//     -eps: epsilon
+//     -delta: lambda
+//     -stats : indicates whether to output comparisons to exact coreness
+//              values
+//     -ins-opt : indicates whether to set lambda such that
+//                (2 + 3 / lambda) = 1.1
+//     -size : option for getting the size of the data structure in bytes
+//     -opt : indicates whether to divide the number of levels by 50; faster
+//     runtime, subtle decrease in accuracy
+//     -init_graph_file: initial graph for which the additional edge updates
+//     occur on; initial graph should follow the same format as the input to -i
+//     (i.e. all edges should be prepended with '+')
+// Note: The static graph is ignored if -i is specified.
+
+#include "ETTree.h"
+
+namespace gbbs {
+template <class Graph>
+double LDS_runner(Graph& G, commandLine P) {
+  // Run LDS
+  timer t; t.start();
+  RunETTree();
+  double tt = t.stop();
+
+  std::cout << "### Running Time: " << tt << std::endl;
+
+  return tt;
+}
+}  // namespace gbbs
+
+generate_symmetric_main(gbbs::LDS_runner, false);
---
+++ gbbs/benchmarks/BatchDynamicConnectivity/EulerTourTree/ETTree.h
@@ -0,0 +1,40 @@
+#include <unordered_set>
+#include <stack>
+
+#include "gbbs/gbbs.h"
+#include "gbbs/dynamic_graph_io.h"
+#include "benchmarks/KCore/JulienneDBS17/KCore.h"
+#include "benchmarks/EdgeOrientation/ParallelLDS/sparse_set.h"
+#include "benchmarks/BatchDynamicConnectivity/SkipList/SkipList.h"
+
+namespace gbbs {
+
+struct ETTree {
+
+    struct ETTreeBase {
+        SkipList::SkipListElement edge; // edge (u, v)
+        SkipList::SkipListElement* twin; // edge (v, u)
+
+        bool skip_mark;
+
+        ETTreeBase() {
+            edge = SkipList::SkipListElement();
+            twin = nullptr;
+
+            skip_mark = false;
+        }
+
+        ETTreeBase(uintE u, uintE v, SkipList::SkipListElement* twin_) {
+                // TODO: make the value take two values: u and v
+                edge = SkipList::SkipListElement(u, nullptr, nullptr, v);
+                skip_mark = false;
+                twin = twin_;
+        }
+    };
+};
+
+void RunETTree() {
+        std::cout << "ET tree" << std::endl;
+}
+
+}  // namespace gbbs
